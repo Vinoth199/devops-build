bd43cebbcf6e8c584781bb317edbea802dbeeb1f
vinoth199 <yuvarajg1908@gmail.com>
2024-08-21 00:08:48 +0530

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,6 @@ LOCAL_DOCKER_COMPOSE_PATH="./docker-compose.yml"
 REMOTE_DOCKER_COMPOSE_PATH="/home/ubuntu/docker-compose.yml"
 SSH_KEY_PATH="/home/vinoth/Zen/caps.pem"
 
-# Ensure the SSH key has the correct permissions
-chmod 400 $SSH_KEY_PATH
-
 # Copy Docker Compose file to EC2 instance
 scp -i $SSH_KEY_PATH $LOCAL_DOCKER_COMPOSE_PATH $EC2_USER@$EC2_HOST:$REMOTE_DOCKER_COMPOSE_PATH
 
