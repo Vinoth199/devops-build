d85a0afc4242aa6148effc13902a5b007c591c62
vinoth199 <yuvarajg1908@gmail.com>
2024-08-20 23:46:22 +0530

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Set variables
-SERVER_IP="ec2-13-202-150-30.ap-south-1.compute.amazonaws.com"
-SSH_USER="ubuntu"
+# Define variables
+EC2_USER="ubuntu"
+EC2_HOST="ec2-13-202-150-30.ap-south-1.compute.amazonaws.com"
 DOCKER_COMPOSE_FILE="docker-compose.yml"
-BUILD_FOLDER="build"
+LOCAL_DOCKER_COMPOSE_PATH="./docker-compose.yml"
+REMOTE_DOCKER_COMPOSE_PATH="/home/ubuntu/docker-compose.yml"
 
-# Copy Docker Compose file and build folder to EC2
-scp -o StrictHostKeyChecking=no ${DOCKER_COMPOSE_FILE} ${SSH_USER}@${SERVER_IP}:/home/${SSH_USER}/
-scp -o StrictHostKeyChecking=no -r ${BUILD_FOLDER} ${SSH_USER}@${SERVER_IP}:/home/${SSH_USER}/
+# Copy Docker Compose file to EC2 instance
+scp -i /home/vinoth/Zen/caps.pem $LOCAL_DOCKER_COMPOSE_PATH $EC2_USER@$EC2_HOST:$REMOTE_DOCKER_COMPOSE_PATH
 
 # SSH into EC2 and run Docker Compose
-ssh -tt -o StrictHostKeyChecking=no ${SSH_USER}@${SERVER_IP} "docker-compose -f /home/${SSH_USER}/${DOCKER_COMPOSE_FILE} up -d"
+ssh -i /home/vinoth/Zen/caps.pem $EC2_USER@$EC2_HOST << 'EOF'
+cd /home/ubuntu
+docker-compose up -d
+EOF
